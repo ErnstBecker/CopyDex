5275a1c1667996e5835e4b643a77898733ec74cb
ErnstBeckerNeto <ernst.becker.neto@gmail.com>
2023-02-11 12:16:56 -0300

    Added the components

---
+++ src/components/LogoPokemon.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link class="routerHome" to="/">
+    <img class="logo" src="../assets/images/logo.png" alt="logo" />
+  </router-link>
+</template>
+
+<style>
+.logo {
+  position: fixed !important;
+  left: 2%;
+  top: 0%;
+  height: 6rem;
+}
+
+.routerHome {
+  position: fixed !important;
+  left: 2%;
+  top: 0%;
+  height: 6rem;
+}
+</style>
