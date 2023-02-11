9c1ad1a771de2399e2edab6d9e66fa43f753df3b
ErnstBeckerNeto <ernst.becker.neto@gmail.com>
2023-02-11 12:16:24 -0300

    Added the main file of Website

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Logo from "../src/components/LogoPokemon.vue";
+</script>
+
+<template>
+  <!-- Logo clicável para retornar ao "Home" (/) -->
+  <Logo />
+  <!-- Visualização das páginas utulizando o "Router"-->
+  <Router-view />
+</template>
+
+<script></script>
+
+<style>
+body {
+  background: url(/src/assets/images/background.png);
+}
+</style>
